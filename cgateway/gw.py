74acf6b67aaea09c66bd2e239c53ed0e7b0ac870
Romans Taranovs <romans.taranovs@rtu.lv>
2014-10-30 06:29:58 +0200

    REF: US #109: As a Cloud Developer I want to receive measurements with precise date stamps DES: A new calss for configuration added. Time now syncs with Cloud server. TimeZone should be set up before.

--- cgateway/gw.py
+++ cgateway/gw.py
@@ -2,7 +2,6 @@
 
 import os
 import sys, getopt
-# import time
 import json
 import requests
 
@@ -14,16 +13,35 @@ from decimal import *
 from time import gmtime, strftime
 from datetime import datetime
 
+url = 'http://officeauthomationservice.cloudapp.net/'
+key = 'QG4WK-X8EGS-NA4UJ-Z4YTC'
+companyId = '1'
 ids = {'D': 3, 'N': 9, 'M': 4, 'U': 6}
 
+class Configuration():
+    def __init__(self):
+        self.id = ""
+        self.timeFromServer = ""
+    def cfgGWTime(self, now_):
+        href = url + 'API/Device/GetServerDateTime'
+        token = ComputeHash(now_, key)
+        authentication = companyId + ":" + token
+        headers = {'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json', 'Authentication': authentication}
+        r = requests.get(href, headers=headers)
+        if r.status_code == 200:
+            self.timeFromServer = r.json()
+            print ("Setting up time to: " + self.timeFromServer)
+            self.command = 'sudo -S date -s "' + self.timeFromServer + '"'
+            os.popen(self.command, 'w').write("123")
+        else:
+            print 'Error in setting time. Server response code: %i' % r.status_code       
+
 def main(argv):
-    url = 'http://officeauthomationservice.cloudapp.net/'
-    # now_ = '2014-08-13T14:06:50.7214802+03:00'
-    # ser = serial.Serial(5)
     hum = ""
     temperature = ""
     movement = ""
     lux = ""
+
     print 'Number of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
 
@@ -31,9 +49,10 @@ def main(argv):
         print 'gw.py <data_to_parse>'
         sys.exit(2)
 
-    # print ser.name
     now_ = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    # line = ser.readline()
+    cfg = Configuration()
+    cfg.cfgGWTime(now_)
+
     temp =argv[0].split("_")
     print (temp)
 
@@ -56,7 +75,7 @@ def main(argv):
         lux = processLuxSensor(temp, len(temp))
         print "Lux: ", lux
 
-    setAlarmState(now_, url, temperature, hum, lux, ids[fromType[0]], movement)
+    setAlarmState(now_, temperature, hum, lux, ids[fromType[0]], movement)
 
 def processPIRSensor(data, dataLen):
     for sensor in data:
@@ -76,13 +95,12 @@ def processDHTSensor(data, dataLen):
         retData = data[1]
     return retData
 
-def setAlarmState(now_, url, temper, humi, luxi, deviceId, move=0):
+def setAlarmState(now_, temper, humi, luxi, deviceId, move=0):    
     href = url + 'api/events/process'
-    companyId = '1'
-    key = 'QG4WK-X8EGS-NA4UJ-Z4YTC'
     token = ComputeHash(now_, key)
     authentication = companyId + ":" + token
     print(authentication)
+    
     headers = {'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json', 'Timestamp': now_, 'Authentication': authentication}
     measurements = []    
     if temper != "":
@@ -116,13 +134,11 @@ def setAlarmState(now_, url, temper, humi, luxi, deviceId, move=0):
     #measurements = [{"EventType":7,"EventValue":temp,"EventTime":now_},{"EventType":6,"EventValue":hum,"EventTime":now_},{"EventType":1,"EventValue":movement,"EventTime":now_}]
 
     print measurements
-    #return 1
 
     payload = {'events': measurements, "deviceId": deviceId}
     print(json.dumps(payload))
     r = requests.post(href, headers=headers, data=json.dumps(payload))
     print (r)
-    # print (r.json())
 
 def ComputeHash(timeStamp, key):
     message = bytes(timeStamp).encode('utf-8')
