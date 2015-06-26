27abdddd3576acb07614c1b8428a689f632abaed
Viktors Jesilevskis <v.jesilevskis@gmail.com>
2015-06-27 00:43:22 +0300

    remove SUDO usage from update scripts

--- update.sh
+++ update.sh
@@ -1,21 +1,21 @@
-sudo rm -rf github
-sudo mkdir github
+rm -rf github
+mkdir github
 cd github
-sudo git clone git://github.com/dtstk/IoT_Acc
+git clone git://github.com/dtstk/IoT_Acc
 cd ..
-sudo rsync -av --progress github/IoT_Acc/cgateway/ cgateway --exclude config.json
+rsync -av --progress github/IoT_Acc/cgateway/ cgateway --exclude config.json
 cd cgateway
 cd RF24
-sudo make install
+make install
 cd ~/IoT_Acc/cgateway
-sudo make
-sudo chmod 777 gw.py
-sudo chmod 777 remote
-sudo chmod 777 register.py
-sudo chmod 777 init.py
-sudo chmod 777 remote.cpp
-sudo chmod 777 launcher.sh
+make
+chmod 777 gw.py
+chmod 777 remote
+chmod 777 register.py
+chmod 777 init.py
+chmod 777 remote.cpp
+chmod 777 launcher.sh
 cd ~/IoT_Acc/
 cp github/IoT_Acc/update.sh update.sh -f -u
-sudo chmod 777 update.sh
+chmod 777 update.sh
 
