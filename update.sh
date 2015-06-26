d0069d44f58c850cafd40471587ca4be1be811a7
Viktors Jesilevskis <v.jesilevskis@gmail.com>
2015-06-26 23:27:10 +0300

    Update script setup exactly directories to locate

--- update.sh
+++ update.sh
@@ -7,15 +7,15 @@ sudo rsync -av --progress github/IoT_Acc/cgateway/ cgateway --exclude config.jso
 cd cgateway
 cd RF24
 sudo make install
-cd ..
+cd ~/IoT_Acc/cgateway
 sudo make
 sudo chmod 777 gw.py
 sudo chmod 777 remote
 sudo chmod 777 register.py
-sudo chmod 666 init.py
-sudo chmod 666 remote.cpp
-sudo chmod 666 launcher.sh
-cd ..
+sudo chmod 777 init.py
+sudo chmod 777 remote.cpp
+sudo chmod 777 launcher.sh
+cd ~/IoT_Acc/
 cp github/IoT_Acc/update.sh update.sh -f -u
 sudo chmod 777 update.sh
 
