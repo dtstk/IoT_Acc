3ec7ed6861769a46ba1365af531e62b5bd51126e
Viktors Jesilevskis <v.jesilevskis@gmail.com>
2015-07-01 09:08:51 +0300

    Send IP of GW on initializing

--- cgateway/init.py
+++ cgateway/init.py
@@ -12,6 +12,7 @@ import serial
 from decimal import *
 from time import gmtime, strftime
 from datetime import datetime
+import socket
 
 class Configuration():
     def __init__(self):
@@ -32,6 +33,8 @@ class Configuration():
         else:
             print 'Error in setting time. Server response code: %i' % r.status_code       
 
+
+
 def main(argv):
 
     print 'Sync PI and server time'       
@@ -43,6 +46,39 @@ def main(argv):
     now_ = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     cfg = Configuration()
     cfg.cfgGWTime(config_data, now_)   
+
+
+
+
+
+    href = config_data["Server"]["url"] + 'api/Device/DeviceConfigurationUpdate'
+    token = ComputeHash(now_, config_data["Server"]["key"])
+    authentication = config_data["Server"]["id"] + ":" + token
+    print(authentication)
+    
+    headers = {'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json', 'Timestamp': now_, 'Authentication': authentication}
+    
+    deviceDetail = {}
+    deviceDetail["DeviceIdentifier"] = "1067"
+    deviceDetail["DeviceType"] = "Custom"
+    deviceDetail["DeviceConfigurations"] = [{'Key':'IPPublic','Value':[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]}]
+
+    payload = {'Device': deviceDetail}
+    print 'Request Content: {0}'.format(json.dumps(payload))
+    r = requests.post(href, headers=headers, data=json.dumps(payload), verify=False)
+
+    if r.status_code == 200:
+       print 'Response Content: {0}'.format(r.content)
+       data = json.loads(r.text)
+       print 'Device configuration Successfully updated'
+
+       return '0'
+    else:
+       print 'Error in setting time. Server response code: {0} {1}'.format(r.status_code, r.content)
+       return '0'
+
+
+
 
 def ComputeHash(timeStamp, key):
     message = bytes(timeStamp).encode('utf-8')
