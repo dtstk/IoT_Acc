4cd12c187bc7afe68e8d6e88362261167e1afc71
Viktors Jesilevskis <v.jesilevskis@gmail.com>
2015-07-02 09:31:03 +0300

    Send 'Alive' event

--- cgateway/gw.py
+++ cgateway/gw.py
@@ -127,6 +127,12 @@ def setAlarmState(config_data, now_, temper, humi, luxi, deviceId, move=0):
         lux["EventValue"] = int(luxi)
         lux["EventTime"] = now_
         measurements.append(lux)
+
+    measure = {}
+    measure["EventType"] = 32
+    measure["EventValue"] = 1
+    measure["EventTime"] = now_
+    measurements.append(measure)
        
     #measurements = [{"EventType":7,"EventValue":temp,"EventTime":now_},{"EventType":6,"EventValue":hum,"EventTime":now_},{"EventType":1,"EventValue":movement,"EventTime":now_}]
 
