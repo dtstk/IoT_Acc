4a734f9959b43785ba59964698ff688533b620df
Viktors Jesilevskis <v.jesilevskis@gmail.com>
2015-07-06 23:43:31 +0300

    Example: Read values from Azure Service bus Queue

--- cgateway/az.py
+++ cgateway/az.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python
 import sys
+import json
 from azure.servicebus import ServiceBusService, Message, Queue
-#from .classes import config_parser
-#import classes.config_parser
 
 def main(argv):
     print 'Starting program...'
 
-    queue_name =argv[0]
+    internalDeviceId = argv[0]
+    json_data=open('config.json')
+    config_data = json.load(json_data)
+    json_data.close()
+
+    externalDeviceId = config_data["Devices"][internalDeviceId]
+    queue_name = 'custom_' + config_data["Server"]["id"] + '_' + config_data["Server"]["Deviceid"] + '_' + externalDeviceId
     print (queue_name)
     
-#   config = ConfigParser()
     bus_service = ServiceBusService( service_namespace='rdciot', shared_access_key_name='RootManageSharedAccessKey', shared_access_key_value='EXeZe7r49jCoDz79fESxtMdXwYU6iQwG1Gbo8J4HXyY=')
     print bus_service
     
